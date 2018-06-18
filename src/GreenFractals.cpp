3b2eef91968cfcd02b4bdeac9f52939931d8794e
ecssiah <ecssiah@gmail.com>
2018-06-18 04:28:19 -0500

    Set up animation option

--- src/GreenFractals.cpp
+++ src/GreenFractals.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <complex>
 #include <vector>
 #include <iostream>
 
@@ -25,38 +26,23 @@ int main(int argc, char** argv)
   Vector3f axis1(1, 1, 0);
   axis1.normalize();
 
-  Vector3f vec(1, 1, 1);
+  Vector3f vec(-1/720.0, -1/24.0, -1/2.0);
   auto vec1(vec);
 
-  FractalInstance::num_points = 2e7;
+  FractalInstance::num_points = 4e6;
   FractalInstance::complex_range = 4.0;
   FractalInstance::default_radius = 2e0;
-  FractalInstance::default_iterations = 2e2;
-
-  FractalInstance fi1(
-    {1, 2/2.0, 2/6.0, 2/24.0, 2/120.0},
-    {1, 2, 3, 4, 5},
-    {0, 0, 2.0, 2.0}
-  );
-
-  ImageGenerator::generate(fi1);
-
-  /* FractalInstance fi2( */
-  /*   {1, 4/16.0, 4/64.0}, */
-  /*   {1, 4, 8} */
-  /* ); */
-
-  /* ImageGenerator::generate(fi2); */
-
-  /* FractalInstance fi3( */
-  /*   {1, 8/16.0, 8/64.0}, */
-  /*   {1, 4, 8} */
-  /* ); */
-
-  /* ImageGenerator::generate(fi3); */
+  FractalInstance::default_iterations = 1e2;
 
   for (auto frame(1); frame <= NUM_FRAMES; ++frame)
   {
+    FractalInstance fi(
+      {vec1.x(), vec1.y(), vec1.z(), -1},
+      {6, 4, 2, 1}
+    );
+
+    ImageGenerator::generate(fi, frame);
+
     theta += delta;
     auto t1(AngleAxis<float>(theta, axis1));
     vec1 = t1 * vec;
